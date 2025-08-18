3e24e69b4d5a0976e7c3591e51bf93d6e9d104d2
Khadka <120006828+khadka27@users.noreply.github.com>
2025-08-18 16:22:12 +0545

    Enhance Next.js configuration to include new external domain for image handling, improving support for verified supplements store.

--- next.config.mjs
+++ next.config.mjs
@@ -41,6 +41,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "https://www.verifiedsupplements.store/",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 }
